a6160604c3bf893003fe953048f57aa9016eb5d2
Guilherme Roberto Utech <utech.br@gmail.com>
2021-04-15 16:21:34 -0300

    URI 2742

---
+++ multiverso_richard.sql
@@ -0,0 +1,6 @@
+SELECT life_registry.name, round((life_registry.omega * 1.618), 3) AS "Fator N" 
+FROM life_registry JOIN dimensions
+ON life_registry.dimensions_id = dimensions.id 
+WHERE dimensions.name IN ('C875', 'C774') 
+AND life_registry.name LIKE 'Richard%'
+ORDER BY life_registry.omega ASC;
