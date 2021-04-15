1b9e3f01552d7d786dfda386221824195f11a25b
Guilherme Roberto Utech <utech.br@gmail.com>
2021-04-15 15:41:44 -0300

    URI 2738

---
+++ concurso.sql
@@ -0,0 +1,7 @@
+SELECT candidate.name,
+cast((((score.math * 2) + 
+(score.specific * 3) + 
+(score.project_plan * 5)) / 10) AS NUMERIC(15, 2)) AS avg
+FROM candidate JOIN score 
+ON candidate.id = score.candidate_id
+ORDER BY avg DESC;
